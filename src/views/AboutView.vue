9a19b94026ba9e92732057e6b2f0ad81ff531b14
Carlos Segura <lithos.contact@gmail.com>
2022-09-27 21:18:17 +0200

    feat(about): add about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
-  <h1>About</h1>
+  <div
+    class="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center"
+  >
+    <h1 class="text-5xl text-indigo-800">About iCompose</h1>
+    <p class="text-lg mt-5 text-slate-800">
+      iCompose is a component portfolio made with Vue 3, Vite, TypeScript and
+      Tailwind CSS.
+    </p>
+    <p class="text-lg mt-5 text-slate-800">
+      Each component have been made from scratch without using third parties
+      libraries. The goal is to create a component framework (such as Vuetify or
+      Bootstrap) and be able to install and use it in other projects.
+    </p>
+  </div>
 </template>
 
 <script lang="ts">
